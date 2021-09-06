42de2b8802f605a57a93c9229c44198a2f8b735e
ninaargade <argade.nina@gmail.com>
2021-09-06 14:38:46 -0700

    Final

--- Janggi.py
+++ Janggi.py
@@ -1,10 +1,5 @@
 # Author: Nina Argade
 # Date: 3/11/2021
-# Description: The following program contains a class that executes the game Janggi, which is the Korean version of
-# chess. The program represents the backend of the game, where there is a way for keeping track of the game board,
-# game pieces, player turns, and game state. There is a method to make a move which takes in algebraic notation
-# coordinates representing spaces on the board. There are many various helper functions that execute game play, and
-# finally, there are methods to verify if a player is in check or checkmate, which indicate if the game has been won.
 
 # noinspection PyUnreachableCode
 class JanggiGame:
