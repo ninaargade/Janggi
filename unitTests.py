c5696b002a87928dfb105bdf27e4c83610e1e58f
ninaargade <71619173+ninaargade@users.noreply.github.com>
2023-12-31 17:06:59 -0800

    Create unitTests.py
    Created unit tests for Janggi.py

---
+++ unitTests.py
@@ -0,0 +1,12 @@
+import unittest
+import Janggi
+
+
+class TestCase(unittest.TestCase):
+    """TestCase class inherits from unittest.TestCase to form different methods as test cases. """
+
+    def test1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
