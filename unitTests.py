927bd8f9ab609d5aaa25091f0b9d54c1941cc1df
Nina Argade <argaden@oregonstate.edu>
2024-01-06 22:38:34 -0600

    check if tests execute in order

--- unitTests.py
+++ unitTests.py
@@ -1,11 +1,12 @@
 import unittest
+unittest.TestLoader.sortTestMethodsUsing = None
 from Janggi import JanggiGame
 
 
 class TestCase(unittest.TestCase):
     """TestCase class inherits from unittest.TestCase to form different methods as test cases. """
 
-    def test_blueGuard(self):
+    def test_redGuard(self):
         j = JanggiGame()
         result = j.make_move('d10', 'd9')
         self.assertTrue(result)
