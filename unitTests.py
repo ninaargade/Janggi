a1078a81bfe9a401d3e20dab7b55bc5cdcfc92b2
Nina Argade <argaden@oregonstate.edu>
2024-01-06 22:10:14 -0600

    test blue guard

--- unitTests.py
+++ unitTests.py
@@ -5,11 +5,10 @@ import Janggi
 class TestCase(unittest.TestCase):
     """TestCase class inherits from unittest.TestCase to form different methods as test cases. """
 
-    def test1(self):
-        pass
-        # j = Janggi()
-        # result = j.make_move('d10', 'd9')
-        # self.assertTrue(result)
+    def test_blueGuard(self):
+        j = Janggi()
+        result = j.make_move('d10', 'd9')
+        self.assertTrue(result)
 
     def test2(self):
         pass
