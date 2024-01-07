5f739205a4eeaf7341a4d7b546c599d7cd19e636
Nina Argade <argaden@oregonstate.edu>
2024-01-06 22:47:34 -0600

    check if blue turn

--- unitTests.py
+++ unitTests.py
@@ -5,15 +5,15 @@ from Janggi import JanggiGame
 class TestCase(unittest.TestCase):
     """TestCase class inherits from unittest.TestCase to form different methods as test cases. """
 
-    def test_1(self):
+    def test_redGuard(self):
         j = JanggiGame()
         result = j.make_move('d10', 'd9')
         self.assertTrue(result)
 
-    def test_2(self):
+    def test_getTurn(self):
         j = JanggiGame()
         result = j.get_turn()
-        self.assertEqual(result, 'Red')
+        self.assertEqual(result, 'Blue')
 
 if __name__ == '__main__':
     unittest.main()
